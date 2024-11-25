70f28165aa806350231ca771d372fc306b48e2ae
Lyu80V <7222707@gmail.com>
2024-11-25 15:04:09 +0300

    Update module_13_5.py

--- module_13_5.py
+++ module_13_5.py
@@ -5,7 +5,7 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
 
-api = '7201079216:AAGijMIsykxlS74Osg113pM0mFLkBVyn9Wo'
+api = ''
 bot = Bot(token=api)
 dp = Dispatcher(bot, storage=MemoryStorage())
 
